4fc731f9835d5e64cef17889a4562f08d8b0926a
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-20 18:35:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,11 +59,11 @@ end
 
 def adding_matz
 
-	programmer_hash = 
- 		{   :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-  :languages => ["LISP", "C"]
-},
+	programmer_hash = 	{   
+	      :yukihiro_matsumoto => {
+           :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -97,7 +97,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     :programmer_hash[alans_new_info] = "GUI"
      
      
 end
