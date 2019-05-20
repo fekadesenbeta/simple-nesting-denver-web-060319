feb60e6cc84313ea4df1fe4058979b30ca848976
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-20 18:24:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,8 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = {  :yukihiro_matsumoto => {
+	programmer_hash = {  
+	  :yukihiro_matsumoto => {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
     }
